d1dac52ae7d4717fba3f52796b8b236d1ffe2577
Kaditcuy <philipukchimaobi@gmail.com>
2023-01-24 01:08:40 +0000

    Added fabric script to compress all webstatic files to an archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+generates a .tgz archive from the contents of the web_static folder
+of the Airbnb clone, using do_pack function
+"""
+from datetime import datetime
+from fabric.api import *
+import os
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents
+    of the web_static folder of this repo.
+    """
+    if not os.path.exists("versions"):
+        os.mkdir("versions")
+
+    date_time = datetime.now().strfttime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(date_time)
+    archive_path = "versions/{}".format(archive_name)
+
+    local("tar -cvzf {} web_static".format(archive_path))
+
+    if os.path.exists(archive_path):
+        return archive_path
+    return None
